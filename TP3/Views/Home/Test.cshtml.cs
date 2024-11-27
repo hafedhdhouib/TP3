f3b8ef244b83308b3a092aa9c68365fbada45ed7
hafedhdhouib <hafedhdhouib@gmail.com>
2024-11-27 20:33:33 +0100

    Ajoutez des fichiers projet.

---
+++ TP3/Views/Home/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TP3.Views.Home
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
