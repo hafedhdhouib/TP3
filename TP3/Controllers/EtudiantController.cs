272f070683d0007fb65641b215b9c518a47b5d10
hafedhdhouib <hafedhdhouib@gmail.com>
2024-11-28 19:25:30 +0100

    v1

---
+++ TP3/Controllers/EtudiantController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP3.Models;
+
+namespace TP3.Controllers
+{
+    public class EtudiantController : Controller
+    {
+        public EtudiantController()
+        {
+            if (Etudiant.Etudiants.Count()==0) 
+            {
+                Etudiant.Etudiants.Add(new Etudiant() {
+                    Matricule="1000",
+                    Adress="SFAX",
+                    FullName="Hafedh Dhouib",
+                    DateNaissance=new DateTime(2000,1,1)
+                });
+            }
+        }
+        public IActionResult Index()
+        {
+            return View(Etudiant.Etudiants);
+        }
+    }
+}
---
+++ TP3/Models/Etudiant.cs
@@ -0,0 +1,12 @@
+﻿namespace TP3.Models
+{
+    public class Etudiant
+    {
+        public string Matricule { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateNaissance { get; set; }
+        public string Adress { get; set; }
+        public static List<Etudiant> Etudiants =
+            new List<Etudiant>();
+    }
+}
