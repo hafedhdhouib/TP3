98abb9820be7f45cb3668017f0d23cfbefbdb063
hafedhdhouib <hafedhdhouib@gmail.com>
2024-12-05 18:46:21 +0100

    seance 3

--- TP3/Controllers/EtudiantController.cs
+++ TP3/Controllers/EtudiantController.cs
@@ -7,19 +7,80 @@ namespace TP3.Controllers
     {
         public EtudiantController()
         {
-            if (Etudiant.Etudiants.Count()==0) 
+            if (Etudiant.Etudiants.Count() == 0)
             {
-                Etudiant.Etudiants.Add(new Etudiant() {
-                    Matricule="1000",
-                    Adress="SFAX",
-                    FullName="Hafedh Dhouib",
-                    DateNaissance=new DateTime(2000,1,1)
+                Etudiant.Etudiants.Add(new Etudiant()
+                {
+                    Matricule = "1000",
+                    Adress = "SFAX",
+                    FullName = "Hafedh Dhouib",
+                    DateNaissance = new DateTime(2000, 1, 1)
                 });
             }
         }
         public IActionResult Index()
         {
             return View(Etudiant.Etudiants);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Etudiant etudiant, string FullName)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Etudiant.Etudiants.Where(x => x.Matricule == etudiant.Matricule).Any())
+                {
+                    ModelState.AddModelError("","Matricule Exist!!");
+                    //ViewBag.error = "matricule existe !!!!";
+                    return View(etudiant);
+                }
+                else
+                {
+                    Etudiant.Etudiants.Add(etudiant);
+                    return RedirectToAction("index");
+                }
+            }
+            return View(etudiant);
+        }
+        public IActionResult Edit(string matricule)
+        {
+            var etud = Etudiant.Etudiants.Where(c => c.Matricule.Equals(matricule)).FirstOrDefault();
+
+            return View(etud);
+        }
+        [HttpPost]
+        public IActionResult Edit(Etudiant etud)
+        {
+            var etudiant = Etudiant.Etudiants.Where(c => c.Matricule.Equals(etud.Matricule)).FirstOrDefault();
+            if (etudiant!=null)
+            {
+                etudiant.FullName = etud.FullName;
+                etudiant.Adress = etud.Adress;
+                etudiant.DateNaissance = etud.DateNaissance;
+            return RedirectToAction("index");
+            }
+            return View(etud);
+        }
+
+        public IActionResult Delete(string matricule)
+        {
+            var etud = Etudiant.Etudiants.Where(c => c.Matricule.Equals(matricule)).FirstOrDefault();
+
+            return View(etud);
+        }
+        [HttpPost]
+        public IActionResult Delete(string matricule,Etudiant e)
+        {
+            var etud = Etudiant.Etudiants.Where(c => c.Matricule.Equals(matricule)).FirstOrDefault();
+            if (etud!=null)
+            {
+                Etudiant.Etudiants.Remove(etud);
+                return RedirectToAction("index");
+            }
+            return View(etud);
         }
     }
 }
--- TP3/Models/Etudiant.cs
+++ TP3/Models/Etudiant.cs
@@ -3,9 +3,9 @@
     public class Etudiant
     {
         public string Matricule { get; set; }
-        public string FullName { get; set; }
-        public DateTime DateNaissance { get; set; }
-        public string Adress { get; set; }
+        public string? FullName { get; set; }
+        public DateTime? DateNaissance { get; set; }
+        public string? Adress { get; set; }
         public static List<Etudiant> Etudiants =
             new List<Etudiant>();
     }
